f83392cbfcf3d9cc4c2ce4810923c57b860f37be
dnhckt <hacketta.t@gmail.com>
2019-06-19 19:30:22 +0100

    Added Rover mutation methods for one iteration (e.g. turn left, turn right, move a step)

--- Main.java
+++ Main.java
@@ -6,31 +6,97 @@ Author: Aidan Hackett
 
 */
 
-/* Map class -- may be needed later
-  =======================
-// public class Map {
-//     int xSize;
-//     int ySize;
 
-//     Map(int xSize, int ySize) {
-
-//     }
-// }
-
-*/
-
-public class Rover {
+ class Rover {
     int xPosition;
     int yPosition;
-    String compassDirection;
+    char compassDirection;
 
-    Rover(int x, int y, String c) {
+    Rover(int x, int y, char c) {
+
         this.xPosition = x;
         this.yPosition = y;
         this.compassDirection = c; 
     }
 
-    public void setPosition(String S) {
+    public void setPosition(String roverAction) {
+        
+        switch (roverAction) {
+            
+            case "L":
+                this.compassDirection = turnCompass(this.compassDirection, false);
+                System.out.println(roverAction);
+            break;
+            
+            case "R":
+                this.compassDirection = turnCompass(this.compassDirection, true);
+                System.out.println(roverAction);
+            break;
+            
+            case "M":
+                roverMove(this.compassDirection);
+                System.out.println(roverAction);
+            break;
+            
+            default:
+                System.out.println("Sorry, invalid input");
+            break;
+        }
+
+    }
+
+    private char turnCompass(char direction, boolean clockwise) {
+        switch (direction) {
+            case 'N':
+                if (clockwise)
+                    direction = 'E';
+                else
+                    direction = 'W';
+            break;
+
+            case 'E':
+                if (clockwise)
+                    direction = 'S';
+                else
+                    direction = 'N';
+            break;
+            
+            case 'S':
+                if (clockwise)
+                    direction = 'W';
+                else
+                    direction = 'E';
+            break;
+            
+            case 'W':
+                if (clockwise)
+                    direction = 'N';
+                else
+                    direction = 'S';
+            break;
+            
+            default:
+            System.out.println("Not a valid compass direction!");
+            break;
+        }        
+        return direction;
+    }
+
+    private void roverMove(char direction) {
+        switch (direction) {
+            case 'N':
+                this.yPosition = this.yPosition + 1; 
+            break;
+            case 'E':
+                this.xPosition = this.xPosition - 1;
+            break;
+            case 'S':
+                this.yPosition = this.yPosition - 1;
+            break;
+            case 'W':
+                this.xPosition = this.xPosition + 1;
+            break;
+        }
     }
 
 }
@@ -38,7 +104,7 @@ public class Rover {
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Mars Rover Calculator!")
+        System.out.println("Welcome to Mars Rover Calculator!");
 
 /* Custom plateau input - for now use 5, 5 */
 /*       
@@ -53,15 +119,19 @@ public class Main {
         System.out.println("You entered" + platX + ", " + playY);
 */  
         /* Set size of plateau */
-        int platX, platY = 5;
+        int platX = 5; 
+        int platY = 5;
         
         /* Set initial rover position */
+        Rover rover = new Rover(0, 0, 'W');
+        System.out.println(rover.xPosition + " " + rover.yPosition + " " + rover.compassDirection);
         
+        /*  Take movement input */
+        rover.setPosition("M");
 
-        /*  Take movement input  */
-    
         /* Provide output */
-  
+        System.out.println(rover.xPosition + " " + rover.yPosition + " " + rover.compassDirection);
+        
     }
 
 }
